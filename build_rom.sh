3fe3cab000d577588a7d81ca79cfd046e778a1ef
Supernova Team ID <frolomose77@gmail.com>
2024-06-03 22:42:08 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,13 +5,13 @@ set -x
 
 # sync rom
 repo init --depth=1 -u //github.com/ProtonPlus-ORG/manifest -b tm-qpr3
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+git clone https://github.com/PixelExperience-Devices/device_xiaomi_begonia --depth 1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex -j$(nproc --all)
+lunch device-buildtype
+m otapackage
 
 # upload rom
 up(){
@@ -19,4 +19,4 @@ up(){
 	# 14 days, 10 GB limit
 }
 
-up out/target/product/mido/*.zip
+up out/target/product/begonia/*.zip
